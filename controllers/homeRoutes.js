493c31d8506f5b53cb9899c99d1fff185f278a49
Samuel Mandel <slman.760@gmail.com>
2022-05-10 13:30:46 -0700

    home routes, initial html routes for testing handlebar.js

---
+++ controllers/homeRoutes.js
@@ -0,0 +1,41 @@
+const router = require('express').Router();
+// const {User, Team, Subscribe} = require('../models');
+// const withAuth = require('../utils/auth');
+
+//render homepage with teams
+router.get('/', async (req, res) => {
+    try {
+        //find all teams, map team data, render on homepage
+        //const teamData = await Team.findAll
+        //const teams = teamData.map((team) => blog.get({plain:true}));
+
+        //render should take in ,{teams, logged_in: req.session.logged_in}
+        res.render('login')
+    }
+    catch(err) {
+        res.status(500).json(err)
+    }
+})
+//router.get('/team/:id', async (req, res)=> {
+// find team data by id
+//query clientside api
+//})
+
+router.get('/profile', withAuth, async (req, res) => {
+    try{
+        //find user data by id
+        //include subscriptions
+        //??query clientside api?
+    } catch (err) {
+        res.status(500).json(err);
+    }
+})
+
+
+//render login if not logged in or profile if logged in
+router.get('/login', (req,res)=> {
+    // if (req.session.logged) {
+    // res.redirect('/profile')
+    // }
+    res.render('login')
+})
