88532b8998120f6a912352ed6bd687063c603c27
Samuel Mandel <slman.760@gmail.com>
2022-05-10 17:46:48 -0700

    add login post route

--- controllers/api/userRoutes.js
+++ controllers/api/userRoutes.js
@@ -15,3 +15,26 @@ router.post('/', async (req, res)=> {
         res.status(400).json(err)
     }
 })
+
+router.post('/login', async (req, res) => {
+    try {
+        const userData = await User.findOne({where: { name: req.body.name}});
+
+        if(!userData) {
+            res.status(400).json({message: 'Incorrect username'});
+            return;
+        }
+        const validPassword = await userData.checkPassword(req.body.password);
+        if(!validPassword){
+            res.status(400).json({message: 'Incorrect password'});
+            return;
+        }
+        req.session.save(() => {
+            req.session.user_id = userData.id;
+            req.session.logged_in = true;
+            res.json({user:userData, message: 'Login Successful'})
+        });
+    } catch(err) {
+        res.status(400).json(err);
+    }
+} )
