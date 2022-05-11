252ced2415626958bc5a52884e6501113e69e2e3
Samuel Mandel <slman.760@gmail.com>
2022-05-11 11:43:22 -0700

    signup route added

--- controllers/homeRoutes.js
+++ controllers/homeRoutes.js
@@ -41,6 +41,9 @@ router.get('/login', (req, res) => {
     }
     res.render('login')
 })
+router.get('/signup', (req, res)=> {
+    res.render('signup')
+});
 
 router.get('/about', (req, res) => {
     res.render('about')
