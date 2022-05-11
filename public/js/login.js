b29f693de3acd89bf808b00b4fb857d8176dae0d
Snehita Kolli <snehitakolli20@gmail.com>
2022-05-10 17:31:56 -0700

    add the ids for login.handlebars

--- public/js/login.js
+++ public/js/login.js
@@ -21,5 +21,5 @@ const loginFormHandler = async (event) => {
 
 document
   .querySelector('.login-form')
-  .addEventListener('submit', loginFormHandler);
+  .addEventListener('login', loginFormHandler);
 
