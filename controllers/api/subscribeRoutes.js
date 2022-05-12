cea01ae437b49f00fe95201d5daa41d4fb4a37cf
Samuel Mandel <slman.760@gmail.com>
2022-05-12 12:12:57 -0700

    implemented nodemailer and sent first email

--- controllers/api/subscribeRoutes.js
+++ controllers/api/subscribeRoutes.js
@@ -1,7 +1,7 @@
 const router = require('express').Router();
 const { Subscribe, User } = require('../../models');
 const withAuth = require('../../utils/auth');
-
+const nodemailer = require('nodemailer');
 router.post('/', withAuth, async (req, res) => {
     try {
         const newSubscribe = await Subscribe.create({
@@ -15,7 +15,26 @@ router.post('/', withAuth, async (req, res) => {
             attributes: {exclude: ['password']}
         })
         const user = userData.get({plain: true});
-        
+        async function main() {
+            // let testAccount = await nodemailer.createTestAccount();
+
+            const transporter = nodemailer.createTransport({
+                service: 'gmail',
+                auth: {
+                    user: 'subscribeomaticmailer@gmail.com',
+                    pass: 'Password1234!'
+                }
+            });
+            let info = await transporter.sendMail({
+                from: '"Subscribe-O-Matic" <subscribeomaticmailer@gmail.com>',
+                to: user.email,
+                subject: "You are Subscribed!",
+                text: "Subscriptions via nodemailer",
+                html: "<b>You are subscribed!</b>",
+            });
+            console.log("Message sent: %s", info.messageId);
+            console.log("Preview URL: %s", nodemailer.getTestMessageUrl(info));
+        } main().catch(console.error);
         console.log(user.email)
         console.log(user)
         res.status(200).json(newSubscribe);
